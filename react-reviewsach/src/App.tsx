ac01cae1ac2e6aea807e7592718d0c2846c3456f
Tien Nguyen <tiennguyenmyjob@gmail.com>
2023-10-03 10:57:12 +0700

    [Feature] Shelf Book

--- react-reviewsach/src/App.tsx
+++ react-reviewsach/src/App.tsx
@@ -7,9 +7,10 @@ import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 import { oktaConfig } from './lib/oktaConfig';
 import {OktaAuth, toRelativeUrl} from '@okta/okta-auth-js';
-import { Security } from '@okta/okta-react';
+import { SecureRoute, Security } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
+import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -50,6 +51,7 @@ export const App = () => {
               path='/login' 
               render={() => <LoginWidget config={oktaConfig} /> } 
             />
+            <SecureRoute path='/shelf'><ShelfPage/></SecureRoute>
           </Switch>
         </div>
         <Footer />
